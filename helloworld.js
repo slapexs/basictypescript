70797b5e959c0ede965379f6d425d0d148f0354d
onze <slapexs@gmail.com>
2022-07-02 14:06:33 +0700

    Hello world

---
+++ helloworld.js
@@ -0,0 +1,6 @@
+var message = "Hello world";
+console.log("Msg: ".concat(message));
+// 
+console.log("This is Stand!!");
+// var money:number = "123" // <- this line type error!
+// console.log(money);
---
+++ helloworld.ts
@@ -0,0 +1,8 @@
+var message:string = "Hello world"
+console.log(`Msg: ${message}`);
+
+// 
+
+console.log("This is Stand!!")
+var money:number = "123" // <- this line type error!
+console.log(money);
