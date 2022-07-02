d8c2f3d5f292cdbc75ff535d4fcc1b779632c045
onze <slapexs@gmail.com>
2022-07-02 14:46:35 +0700

    Basic Operator

---
+++ Operator.ts
@@ -0,0 +1,14 @@
+let x:number = 5+1
+let a = 10
+let b = "10"
+console.log(x); // 6
+x++
+console.log(x); // 7
+x--
+console.log(x); // 6
+
+// console.log(a == b); // <- type error
+// console.log(a === b); // <- type error
+
+console.log(1 && 1); // 1
+console.log(1 || 0); // 1
