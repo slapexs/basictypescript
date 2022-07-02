1f8ab216889895a33dff4e1229d5a3a13054ddb0
onze <slapexs@gmail.com>
2022-07-02 14:28:10 +0700

    Data types

---
+++ dataType.ts
@@ -0,0 +1,21 @@
+/*
+    1. Any
+    2. Built-in type
+    3. User-defined type
+*/
+
+var myName: string = "Sonas";
+console.log(myName);
+console.log(typeof myName);
+
+var money: number = 100;
+console.log(money);
+console.log(typeof money);
+
+var isTrue: boolean;
+
+var i;
+
+console.log(null === undefined);
+console.log(null == undefined);
+console.log(null === null);
